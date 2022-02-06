74294c7bf7c8e9a4c11e6ef763d14d3fe92a7bc4
Aaron Amberman <aaronamberman@gmail.com>
2022-02-06 17:04:25 -0500

    Initial commit

---
+++ WPF.Themes/WPF.Themes/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.Themes
+{
+    /*
+     * A good spot to look at how themeing can be done and how to build XAML files, examples, etc.
+     * https://github.com/MahApps/MahApps.Metro
+     * 
+     * Look here for example of ListViews...
+     * https://github.com/MahApps/MahApps.Metro/tree/develop/src/MahApps.Metro/Styles
+     * 
+     * Also Xceed toolkit...
+     * https://github.com/xceedsoftware/wpftoolkit/tree/master/ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit
+     */
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
