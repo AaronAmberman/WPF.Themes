eec7a3bc310c3828aab58420b8f7b0c86145dcae
Aaron Amberman <aaronamberman@gmail.com>
2022-02-11 21:29:37 -0500

    Minor clean up

--- WPF.Themes/WPF.Themes/MainWindow.xaml.cs
+++ WPF.Themes/WPF.Themes/MainWindow.xaml.cs
@@ -1,44 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace WPF.Themes
 {
-    /*
-     * A good spot to look at how themeing can be done and how to build XAML files, examples, etc.
-     * https://github.com/MahApps/MahApps.Metro
-     * 
-     * Look here for example of ListViews...
-     * https://github.com/MahApps/MahApps.Metro/tree/develop/src/MahApps.Metro/Styles
-     * 
-     * Also Xceed toolkit...
-     * https://github.com/xceedsoftware/wpftoolkit/tree/master/ExtendedWPFToolkitSolution/Src/Xceed.Wpf.Toolkit
-     * 
-     * MaterialDesign Toolkit...
-     * https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit
-     * https://github.com/MaterialDesignInXAML/MaterialDesignInXamlToolkit/blob/master/MaterialDesignThemes.Wpf/Themes/MaterialDesignTheme.ListView.xaml
-     * 
-     * WPF examples...
-     * https://docs.microsoft.com/en-us/previous-versions/dotnet/netframework-3.0/aa970773(v=vs.85)
-     * 
-     * Colors...
-     * https://docs.microsoft.com/en-us/dotnet/api/system.windows.media.colors?view=windowsdesktop-6.0
-     */
-
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
